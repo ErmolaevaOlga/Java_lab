09bc50ce8b5988a328192bcb37bcd5c9734e3854
Olga Ermolaeva <photo-deti@mail.ru>
2023-06-20 14:56:28 +0700

    Добавление кода проекта

---
+++ src/errors/errors.py
@@ -0,0 +1,6 @@
+class InvalidCommandException(Exception):
+    """Класс исключений при вводе комманд пользователем"""
+
+
+class StorageAccessException(Exception):
+    """Класс исключений при работе с хранилищем данных"""
